7bb920aeca1deb89afb4f33b16fa685d9dc798b4
theody85 <gyambrahtheodora.8@gmail.com>
2024-11-21 19:27:26 +0000

    improve ui on login & signup pages

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -32,60 +32,76 @@ const LoginForm = () => {
 
   return (
     <div
-      className="log-in flex flex-col justify-center items-center h-screen text-xs bg-cover bg-center"
+      className="log-in flex flex-col justify-center items-center h-screen bg-cover bg-center"
       style={{ backgroundImage: "url('./src/assets/images/studio2.jpg')" }}
     >
-      <div className="max-w-sm w-full bg-transparent shadow-lg rounded-lg p-6 border border-black">
-        <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
-          <div className="flex flex-col gap-3">
-            <h1 className="flex justify-center font-extrabold">User Login</h1>
-            <p className="flex justify-center font-bold">
-              Hey! Enter your details to sign in to your account
-            </p>
-
-            <input
-              className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
-              type="text"
-              placeholder="Email"
-              required
-              name="email"
-            />
+      <div className="max-w-lg w-full  bg-blend-overlay shadow-lg rounded-lg p-10 border  bg-white/20 backdrop-blur-lg">
+        <form className="flex flex-col gap-3 " onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-6">
             <div>
-              <input
-                className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
-                type="password"
-                placeholder="Password"
-                required
-                name="password"
-              />
+              <h1 className="flex justify-center text-3xl font-bold">
+                Welcome Back!
+              </h1>
+              <p className="flex justify-center ">
+                Hey! Enter your details to sign in to your account
+              </p>
             </div>
+            <div className="flex flex-col gap-y-4">
+              <input
+                className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-green-500 "
+                type="text"
+                placeholder="Email"
+                required
+                name="email"
+              />
+              <div>
+                <input
+                  className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-green-500"
+                  type="password"
+                  placeholder="Password"
+                  required
+                  name="password"
+                />
+              </div>
 
-            <span className="font-bold">
-              <input type="checkbox" /> Remember me
-            </span>
-            <button
-              type="submit"
-              className="flex justify-center bg-green-500 p-2 rounded-lg"
-              disabled={loading}
-            >
-              {loading ? "Logging in..." : "Login"}
-            </button>
+              <span className="font-bold">
+                <input type="checkbox" /> Remember me
+              </span>
+              <button
+                type="submit"
+                className="flex justify-center bg-green-500 p-2 rounded-lg text-white font-bold"
+                disabled={loading}
+              >
+                {loading ? "Logging in..." : "Login"}
+              </button>
+            </div>
             <div>
-              <span className="flex justify-center mb-4">or sign in with</span>
+              <span className="flex justify-center mb-4 font-bold">
+                or sign in with
+              </span>
               <div className="flex font-bold gap-3">
-                <button type="button" className="border rounded-md p-2 w-32">
+                <button
+                  type="button"
+                  className="border rounded-md p-2 w-32 bg-white shadow"
+                >
                   <span className="mr-1">
                     <i className="fa-brands fa-google"></i>
                   </span>
                   Google
                 </button>
-                <button type="button" className="border rounded-md p-2 w-32">
+                <button
+                  type="button"
+                  className="border rounded-md p-2 w-32 bg-white shadow"
+                >
                   <span className="mr-1">
                     <i className="fa-brands fa-apple"></i>
                   </span>
                   AppleID
                 </button>
-                <button type="button" className="border p-2 rounded-md w-32">
+                <button
+                  type="button"
+                  className="border rounded-md p-2 w-32 bg-white shadow"
+                >
                   <span className="mr-1">
                     <i className="fa-brands fa-facebook"></i>
                   </span>
@@ -94,7 +110,7 @@ const LoginForm = () => {
               </div>
               <div className="flex justify-center mt-8">
                 <span>Don't have an account?</span>
-                <Link to="/SignupForm">
+                <Link to="/signup">
                   <span className="ml-2 text-green-600 font-extrabold hover:underline">
                     Sign Up
                   </span>
--- src/components/forms/SignupForm.jsx
+++ src/components/forms/SignupForm.jsx
@@ -47,105 +47,103 @@ const SignupForm = () => {
 
   return (
     <div
-      className="log-in flex flex-col justify-center items-center h-screen text-xs bg-cover bg-center"
+      className="log-in flex flex-col justify-center items-center h-screen  bg-cover bg-center"
       style={{ backgroundImage: `url(${studioBackground})` }}
     >
-      <div className="text-black flex flex-col justify-center items-center h-[60vh]">
-        <div className="max-w-sm w-full shadow-lg border rounded-lg p-6 bg-transparent my-4 md:my-11">
-          <form onSubmit={handleSubmit}>
-            <h1 className="flex justify-center mb-3 text-lg">Register With</h1>
-            <div className="space-y-4">
-              <div>
-                <label htmlFor="firstName" className="block mb-1">
-                  First Name
-                </label>
-                <input
-                  id="firstName"
-                  className="w-full bg-white border p-2 rounded-md"
-                  type="text"
-                  placeholder="Enter your first name"
-                  required
-                  name="firstName"
-                />
-              </div>
-              <div>
-                <label htmlFor="lastName" className="block mb-1">
-                  Last Name
-                </label>
-                <input
-                  id="lastName"
-                  className="w-full bg-white border p-2 rounded-md"
-                  type="text"
-                  placeholder="Enter your last name"
-                  required
-                  name="lastName"
-                />
-              </div>
+      <div className="max-w-lg w-full  bg-blend-overlay shadow-lg rounded-lg p-10 border  bg-white/20 backdrop-blur-lg">
+        <form onSubmit={handleSubmit}>
+          <h1 className="flex justify-center text-3xl font-bold">Join Us!</h1>
+          <div className="flex flex-col gap-6">
+            <div>
+              <label htmlFor="firstName" className="block mb-1">
+                First Name
+              </label>
+              <input
+                id="firstName"
+                className="w-full bg-white border p-2 rounded-md"
+                type="text"
+                placeholder="Enter your first name"
+                required
+                name="firstName"
+              />
+            </div>
+            <div>
+              <label htmlFor="lastName" className="block mb-1">
+                Last Name
+              </label>
+              <input
+                id="lastName"
+                className="w-full bg-white border p-2 rounded-md"
+                type="text"
+                placeholder="Enter your last name"
+                required
+                name="lastName"
+              />
+            </div>
 
-              <div>
-                <label htmlFor="email" className="block mb-1">
-                  Email
-                </label>
-                <input
-                  id="email"
-                  className="w-full bg-white border p-2 rounded-md"
-                  type="email"
-                  placeholder="Enter your email"
-                  required
-                  name="email"
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className="block mb-1">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  className="w-full bg-white border p-2 rounded-md"
-                  type="password"
-                  placeholder="Enter password"
-                  required
-                  name="password"
-                />
-              </div>
-              <div>
-                <label htmlFor="confirmPassword" className="block mb-1">
-                  Confirm Password
-                </label>
-                <input
-                  id="confirmPassword"
-                  className="w-full bg-white border p-2 rounded-md"
-                  type="password"
-                  placeholder="Confirm password"
-                  required
-                  name="confirmPassword"
-                />
-              </div>
+            <div>
+              <label htmlFor="email" className="block mb-1">
+                Email
+              </label>
+              <input
+                id="email"
+                className="w-full bg-white border p-2 rounded-md"
+                type="email"
+                placeholder="Enter your email"
+                required
+                name="email"
+              />
             </div>
-            <button
-              type="submit"
-              className="w-full mt-6 bg-green-500 p-2 rounded-lg text-white hover:bg-gray-700 duration-300"
-              disabled={loading}
-            >
-              {loading ? "Registering..." : "Sign Up"}
-            </button>
-            <div className="text-xs mt-2 text-center">
-              By creating an account you agree to the{" "}
-              <Link to="/PrivacyPolicy">
-                <span className="font-bold underline">Terms and Services</span>
-              </Link>
-              .
+            <div>
+              <label htmlFor="password" className="block mb-1">
+                Password
+              </label>
+              <input
+                id="password"
+                className="w-full bg-white border p-2 rounded-md"
+                type="password"
+                placeholder="Enter password"
+                required
+                name="password"
+              />
             </div>
-            <div className="flex justify-center mt-4 text-sm">
-              <span>Already have an account?</span>
-              <Link to="/LoginForm">
-                <span className="text-green-600 ml-2 hover:underline">
-                  Login
-                </span>
-              </Link>
+            <div>
+              <label htmlFor="confirmPassword" className="block mb-1">
+                Confirm Password
+              </label>
+              <input
+                id="confirmPassword"
+                className="w-full bg-white border p-2 rounded-md"
+                type="password"
+                placeholder="Confirm password"
+                required
+                name="confirmPassword"
+              />
             </div>
-          </form>
-        </div>
+          </div>
+          <button
+            type="submit"
+            className="w-full mt-6 bg-green-500 p-2 rounded-lg text-white hover:bg-gray-700 duration-300 font-bold"
+            disabled={loading}
+          >
+            {loading ? "Registering..." : "Sign Up"}
+          </button>
+          <div className=" mt-2 text-center">
+            By creating an account you agree to the{" "}
+            <Link to="/PrivacyPolicy">
+              <span className="font-bold underline">Terms and Services</span>
+            </Link>
+            .
+          </div>
+          <div className="flex justify-center mt-4 ">
+            <span>Already have an account?</span>
+            <Link to="/login">
+              <span className="text-green-600 ml-2 font-extrabold hover:underline">
+                Login
+              </span>
+            </Link>
+          </div>
+        </form>
       </div>
     </div>
   );
